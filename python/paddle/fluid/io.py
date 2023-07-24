420025bb3eee5a9bf40ec852d304ff0a00104616
xiaoxiaohehe001 <hiteezsf@163.com>
2023-07-24 18:12:17 +0800

    fix_softmax

--- python/paddle/fluid/io.py
+++ python/paddle/fluid/io.py
@@ -297,9 +297,9 @@ def save_inference_model(
 
         main_program.desc.flush()
 
-        main_program = main_program._prune_with_input(
-            feeded_var_names=feeded_var_names, targets=target_vars
-        )
+        # main_program = main_program._prune_with_input(
+        #     feeded_var_names=feeded_var_names, targets=target_vars
+        # )
         main_program = main_program._inference_optimize(prune_read_op=True)
         fetch_var_names = [v.name for v in target_vars]
 
